3f5e6465fffeb744ade6fc3a930e86d634971821
Athos627 <kocher21@yandex.ru>
2025-07-23 18:29:49 +0300

    fix brain-games.js and cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync'
 
 export const askName = () => {
-    var userName = readlineSync.question('May I have your name? ')
-    console.log('Hello, ' + userName + '!')
+  var userName = readlineSync.question('May I have your name? ')
+  console.log('Hello, ' + userName + '!')
 }
