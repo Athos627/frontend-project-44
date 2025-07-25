2228442274a85f5535fee3dd4506c8bf95f2f3b4
Athos627 <kocher21@yandex.ru>
2025-07-25 16:37:20 +0300

    refactoring brain-games.js and cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,11 @@
 import readlineSync from 'readline-sync'
 
-export const askName = () => {
+let globalName
+
+export const greetings = () => {
+  console.log('Welcome to the Brain Games!')
   const userName = readlineSync.question('May I have your name? ')
   console.log('Hello, ' + userName + '!')
+  globalName = userName
+  return globalName
 }
