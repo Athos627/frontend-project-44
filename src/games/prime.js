55d25b82d6e2a6b5e4fb570a0e314009b05b3bde
Athos627 <kocher21@yandex.ru>
2025-08-21 22:03:54 +0300

    modified prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -13,19 +13,18 @@ export const askQuestion = () => {
   return answer
 }
 
-const isPrime = (num) => {
-  if (num <= 1) {
-    return false
-  }
-  for (let i = 2; i <= Math.sqrt(num); i += 1) {
-    if (num % i === 0) {
+export const checkAnswer = (answer) => {
+  const isPrime = (num) => {
+    if (num <= 1) {
       return false
     }
+    for (let i = 2; i <= Math.sqrt(num); i += 1) {
+      if (num % i === 0) {
+        return false
+      }
+    }
+    return true
   }
-  return true
-}
-
-export const checkAnswer = (answer) => {
   const correctAnswer = isPrime(randomNumber)
   if (correctAnswer === true && answer === 'yes') {
     console.log('Correct!')
